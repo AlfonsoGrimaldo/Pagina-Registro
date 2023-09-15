c31e383d8384da4564b8bc23d4e9f1d9673c3eb0
AlfonsoGrimaldo <48926464+AlfonsoGrimaldo@users.noreply.github.com>
2023-09-15 13:38:41 -0600

    Fixes registrar.php

--- registrar.php
+++ registrar.php
@@ -1,17 +1,17 @@
 <?php
 include("con_db.php");
 if($conex){
-    echo "conexion exitosa a la base de datos"
+    echo "conexion exitosa a la base de datos";
 } else {
-    echo "conexion fallida a la base de datos"
+    echo "conexion fallida a la base de datos";
 }
 if(isset($_POST['register'])){
     if(strlen($_POST['name']) >= 1 && strlen($_POST['email']) >= 1 && strlen($_POST['password']) >= 1){
         $name = trim($_POST['name']);
         $email = trim($_POST['email']);
         $password = $_POST['password'];
-        $fechareg = date("d/m/y");
-        $consulta = "INSERT INTO 'datos'('id', 'nombre', 'email', 'password', 'fecha_reg') VALUES ('$name','$email','$password','$fechareg')";
+        $fechareg = date("Y-m-d");
+        $consulta = "INSERT INTO 'datos'('nombre', 'email', 'password', 'fecha_reg') VALUES ('$name','$email','$password','$fechareg')";
         $resultado = mysqli_query($conex, $consulta);
         if($resultado){
             ?>
